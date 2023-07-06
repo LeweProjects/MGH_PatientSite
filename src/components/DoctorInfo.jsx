9cec57ad345d8598c3858b4ccf463c38c14b0733
lewe <loewiayon12@gmail.com>
2023-07-06 19:06:06 +0800

    latest

--- src/components/DoctorInfo.jsx
+++ src/components/DoctorInfo.jsx
@@ -4,11 +4,14 @@ import doc from './images/doc.jpg'
 const DoctorInfo = () => {
 
   return (
-    <div className="back p-24 flex space-x-20">
-      <img src={doc} alt="" className="w-[20%]"/>
-
-      <div className="absolute bg-white p-20 w-[60%]">
-
+    <div className="back p-24 flex justify-evenly space-x-20">
+      
+      <img src={doc} alt="" className="w-72 h-72"/>
+      
+      <div className="bg-white p-20 w-[80%] h-full text-4xl shadow-[]">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aliquid est 
+          magnam ipsum nesciunt illum doloremque laudantium necessitatibus corporis delectu
+          s, iure repudiandae beatae doloribus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, excepturi ducimus repellat obcaecati tempora exercitationem deleniti magni laborum eligendi quidem tenetur doloremque voluptatem ipsam quas recusandae ipsum maxime dolore quae repudiandae sint! Illum voluptates nesciunt minus cum, cupiditate accusantium quidem voluptatum quisquam mollitia aliquam quibusdam ipsa placeat nihil aliquid dolorem deleniti nam repudiandae et quaerat. Necessitatibus quia suscipit porro quam. Dolor perspiciatis eligendi voluptatem facere a. Accusamus excepturi ut necessitatibus obcaecati, molestiae recusandae laboriosam hic aliquid. Alias deleniti reprehenderit magni aspernatur excepturi illo, fuga nemo necessitatibus nam officia, ab ipsa placeat saepe velit laboriosam nostrum nesciunt nulla iusto qui nobis!
       </div>
     </div>
   );
